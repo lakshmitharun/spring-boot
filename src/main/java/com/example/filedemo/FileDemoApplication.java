015b5e15b8ee09153f711885816b3fb77af98ebd
ponnl <lakshmitharun.ponnam@in.pega.com>
2018-10-15 14:39:46 +0530

    as

--- src/main/java/com/example/filedemo/FileDemoApplication.java
+++ src/main/java/com/example/filedemo/FileDemoApplication.java
@@ -2,13 +2,16 @@ package com.example.filedemo;
 
 import com.example.filedemo.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
+@ServletComponentScan
 public class FileDemoApplication {
 
 	public static void main(String[] args) {
